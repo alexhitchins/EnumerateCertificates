e6f27183fef66f0b7c92624a662d49534a1bf4ac
Alex Hitchins <alex@alexhitchins.com>
2014-04-08 16:22:40 +0100

    Initial Commit

---
+++ EnumerateCertificates/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumerateCertificates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            X509Store store = new X509Store("My");
+
+            store.Open(OpenFlags.ReadOnly);
+
+            foreach (X509Certificate2 mCert in store.Certificates)
+            {
+                Console.WriteLine( mCert.Thumbprint );
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
